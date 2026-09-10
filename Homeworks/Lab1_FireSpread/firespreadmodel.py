99aad39af77c8808b0d7932433a9dc8cdb3912d7
Jackson <jtfenner@umich.edu>
2026-09-10 10:13:11 -0400

    finished test case

--- Homeworks/Lab1_FireSpread/firespreadmodel.py
+++ Homeworks/Lab1_FireSpread/firespreadmodel.py
@@ -1,13 +1,10 @@
 '''
 The following code is used to execute Lab01 in for CLaSP410 at University of Michigan
-
-
-
 '''
-
 #Import required libraries
 import matplotlib.pyplot as plt
 import numpy as np
+
 
 
 #Declare constants to use throughout the model testing
@@ -22,18 +19,21 @@ BARE = 1
 FORESTED = 2
 ON_FIRE = 3
 
-'''Task 1. Build the model as described in the next section.
-Show the results of this test in your Methodology section. This cycle where
-we develop and test is called validation. Discuss your implementation and
-design in the Methodology section of your report. Implementation and design
-means how you built your code AND the decisions you made to build and test
-your code. Is everything in a single script? Did you create functions? How
-did you design your code to make it easy to test the output?'''
 
 def initialize_model_array(init_array, times):
     '''
     Take initial coniditions and create buffer around (insert row before/after and col before/after provided array) and add all timesteps needed
     Return that buffered array
+
+    --------------
+        INPUTS
+    --------------
+        init_array:
+            2D Array of Initial State of Model
+        times:
+            Number of times to run the simulation (length of final array will be times-1 as it is 0 indexed)
+
+
     '''
     #Initialize a num_x by num_y by num_times forest grid with int dtype 
     #Real forest is surrounded by 'Bare' Values
@@ -46,19 +46,21 @@ def initialize_model_array(init_array, times):
     #   [ 1, 1, 1, 1, 1 ]
     #
 
+    #get initial array shape to define final array
     shape = init_array.shape
     #create an array that has two more rows and columns than the initial array
-    buffered_array = np.zeros((times,shape[1]+2,shape[2]+2), dtype=int)
+    buffered_array = np.zeros((times,shape[0]+2,shape[1]+2), dtype=int)
     #set the first and last rows as 'BARE'
     buffered_array[:,0,:] = buffered_array[:,-1,:] = BARE
     #set the first and last columns as 'BARE'
     buffered_array[:,:,0] = buffered_array[:,:,-1] = BARE
     #set the interior values of the array as the values of the inital array
-    buffered_array[0,slice(1,shape[1]+1),slice(1,shape[2]+1)] = init_array
+    buffered_array[0,slice(1,shape[0]+1),slice(1,shape[1]+1)] = init_array
 
     #return the buffered array
     return buffered_array
 
+#Function to Get Orthogonal Neighbors to 
 def get_neighbors(row, col):
     '''
     Returns touple of coordinate values touching a central point (no diagonals). 
@@ -104,16 +106,15 @@ def get_neighbors(row, col):
 
 # Create an initial frame for the tests 
 first_forest = np.ones((num_x,num_y), dtype=int) * 2 
-print(first_forest)
+print("initial conditions:\n" , first_forest)
 #create a 3x3 'forest' with P_SPREAD = 1, 0 initial bare spots, and 1 cell on fire (in the center)
-forest = initialize_model_array(first_forest, 2)
-print(forest)
+forest = initialize_model_array(first_forest, num_times)
 #Set center on fire (remember that array dimensions are now '1' indexed
 forest[0, 2, 2] = ON_FIRE
-
-
+#Use the actual array shape to define number of timesteps
+times = forest.shape[0]
 #Loop through each model time step (from the first to one before the last)
-for time_step in range(num_times - 1):
+for time_step in range(times - 1):
     #get the current model output as a 2D array (fixed time)
     curr_forest = forest[time_step, :, :]
     #create a predicted model output starting from the curr_forest
@@ -136,7 +137,6 @@ for time_step in range(num_times - 1):
                     if curr_forest[coord] == FORESTED:
                         if np.random.rand() < P_SPREAD:
                             pred_forest[coord] = ON_FIRE
-
     #Set each initally burning spot to BARE
     for coord in burning_spots:
         pred_forest[coord] = BARE
